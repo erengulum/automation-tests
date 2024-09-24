2ef9bc4778c282441d062ee5b2b159c64b7cbe73
ErenGulum <erengulum834@gmail.com>
2024-09-24 23:24:31 +0200

    first commit

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,60 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    //we first need a webdriver
+    private WebDriver driver;
+
+    public LoginPage() {
+    }
+
+    // By is locator mechanism to interact with elements. It uses attribute of elements
+    private final By usernameField = By.id("user-name");
+
+    private final By passwordField = By.id("password");
+
+    private final By loginButton = By.id("login-button");
+
+    // Using data-test attribute for error message
+    private final By errorMessage = By.cssSelector("[data-test='error']");
+    //alternative:
+    //By errorMessage = By.xpath("//*[@data-test='error']");
+
+
+
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+
+    public void enterUsername(String username){
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void enterPassword(String password){
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLoginButton(){
+      driver.findElement(loginButton).click();
+    }
+
+    public String getErrorMessage(){
+        return driver.findElement(errorMessage).getText();
+    }
+
+    public void removeUsername(){
+        driver.findElement(usernameField).clear();
+    }
+
+    public void removePasswordField(){
+        driver.findElement(passwordField).clear();
+    }
+
+
+
+
+}
---
+++ src/test/java/tests/SauceLoginTest.java
@@ -0,0 +1,97 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pages.LoginPage;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SauceLoginTest {
+
+
+    private static final Logger log = LoggerFactory.getLogger(SauceLoginTest.class);
+    private WebDriver driver;
+
+    private LoginPage loginPage;
+
+
+    //Junit 4deki karsiligi da @Before . Her testten once calistirilir
+    @BeforeEach
+    public void setup(){
+
+        // Step 1: If you use Mac, Initialize the SafariDriver (no need to download a driver)
+        // WebDriver driver = new SafariDriver();
+
+        // Step 1: Use WebDriverManager to automatically download and setup the correct ChromeDriver version
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver();
+        //maximize the screen
+        driver.manage().window().maximize();
+
+        // Navigate to the SauceDemo login page
+        driver.get("https://www.saucedemo.com/");
+
+        loginPage = new LoginPage(driver);
+
+
+    }
+
+    @AfterEach
+    public void finish(){
+        if(driver !=null)
+            driver.quit();
+    }
+
+    @Test
+    public void testSuccessfullLogin(){
+        loginPage.enterUsername("standard_user");
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLoginButton();
+    }
+
+
+    @Test
+    public void emptyUsername(){
+        loginPage.clickLoginButton();
+        Assertions.assertTrue(loginPage.getErrorMessage().contains("Epic sadface: Username is required"));
+
+        loginPage.enterPassword("test");
+        loginPage.clickLoginButton();
+        Assertions.assertTrue(loginPage.getErrorMessage().contains("Epic sadface: Username is required"));
+
+
+        loginPage.enterUsername("test");
+        loginPage.removePasswordField();
+        loginPage.clickLoginButton();
+        Assertions.assertTrue(loginPage.getErrorMessage().contains("Epic sadface: Password is required"));
+
+    }
+
+    @Test
+    public void invalidCredentials(){
+        loginPage.enterUsername("test123");
+        loginPage.enterPassword("test123");
+        loginPage.clickLoginButton();
+        Assertions.assertTrue(loginPage.getErrorMessage().contains("Epic sadface: Username and password do not match any user in this service"));
+    }
+
+
+    @Test
+    public void isLoginSuccessfull(){
+        loginPage.enterUsername("standard_user");
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLoginButton();
+
+    }
+
+
+
+
+
+}
